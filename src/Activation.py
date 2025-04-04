fix: add type casting, clipping value, and change function to handle overflow

src/Activation.py
@@ -79,7 +79,7 @@ class SoftmaxActivationFunction(ActivationFunction):
 
 class LeakyActivationFunction(ActivationFunction):
     def fn(N):
-        return np.maximum(N, 0.01 * N)
+        return np.where(N > 0, N, 0.01 * N)    
 
     def do_ds(N):
         return np.where(N > 0, 1, 0.01)
@@ -92,12 +92,14 @@ class SeluActivationFunction(ActivationFunction):
     def fn(N):
         scale = 1.0507
         alpha = 1.67326
-        return np.maximum(scale * N, scale * alpha * (np.exp(N) - 1))
+        safe_N = np.clip(N, a_min=-100, a_max=50) # handle overflow
+        return np.where(N > 0, scale * N, scale * alpha * (np.exp(safe_N) - 1))
 
     def do_ds(N):
         scale = 1.0507
         alpha = 1.67326
-        return np.where(N > 0, scale, scale * alpha * (np.exp(N)))
+        safe_N = np.clip(N, a_min=-100, a_max=50) # handle overflow
+        return np.where(N > 0, scale, scale * alpha * np.exp(safe_N))
 
     def name():
         return "selu"

src/Layer.py
@@ -28,7 +28,7 @@ class Layer:
     ):
         self.activation = activation
         self.initializer = initializer
-        self.weight = initializer.init(shape)
+        self.weight = initializer.init(shape).astype(np.float64)
         self.bias = np.zeros(shape[1])
         self.regularizer = regularizer
         self.alpha_regularizer = alpha_regularizer
@@ -37,6 +37,7 @@ class Layer:
         )  # Apply RMSNorm if enabled
 
     def run(self, inputs: np.ndarray) -> np.ndarray:
+        inputs = np.clip(inputs, -1e3, 1e3)
         self.input = inputs
         self.linear_combinations = np.matmul(inputs, self.weight) + self.bias
 
@@ -51,7 +52,6 @@ class Layer:
     def update_gradient(self, delta: np.ndarray, ancestor: "Layer"):
         delta = delta.dot(ancestor.weight.T)
         delta = delta * self.activation.do_ds(self.linear_combinations)
-
         self.weight_gradient = np.dot(self.input.T, delta)
         self.bias_gradient = np.sum(delta, axis=0)
         if self.regularizer != None: